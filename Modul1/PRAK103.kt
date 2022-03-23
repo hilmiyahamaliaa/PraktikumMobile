188fed24afbf7d8292a6a87924ac637929474f91
hilmiyahamaliaa <101669741+hilmiyahamaliaa@users.noreply.github.com>
2022-03-23 16:50:26 +0800

    Update PRAK103.kt

--- Modul1/PRAK103.kt
+++ Modul1/PRAK103.kt
@@ -1,11 +1,14 @@
 fun main() {
     println("Input Bilangan : ")
     val x = readLine()!!.toInt()
+    var y = 0
     val i : Int = x
-    for (i in 1..8){
-        val angka = x*i
+    var angka = i
+    while(y<5){
+        angka += i
         if(angka%2==0 || angka%3==0) {
             print("$angka ")
+            y++
         }
     }
 }
